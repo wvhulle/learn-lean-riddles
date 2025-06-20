e6fe6b87bfd096bfce8632282ef6cdda139f0680
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-06-20 13:15:09 +0200

    Improve documentation and simplify jealous mathematician

--- README.md
+++ README.md
@@ -2,19 +2,63 @@
 
 This is a collection of riddles and puzzles implemented in Lean 4 for a workshop given on 17th of July 2025 at Ghent.
 
-## Lean 4
+_**Note**: This is a work in progress. The code is not complete yet, but the puzzles are solvable.
+
+## Getting started (for new users)
+
+If you have never used Lean before, you will have to install some system dependencies and tools to get started.
+
+### Toolchain
+
+Install [`elan`](https://github.com/leanprover/elan), to manage Lean toolchains. It provides the `lean` and `lake` commands. `lake` is similar to cargo and `lean` is similar to `rustc`. To start a new project in the current directory:
+
+```bash
+lake init
+```
+
+### Project structure
 
 Lean code is written in side the files (indirectly) referenced in the `lakefile.toml` file.
 
-## Standard library
+### General documentation
+
+The [reference manual](https://lean-lang.org/doc/reference/latest/) contains information about the core language.
+
+### Standard library
+
+If you need data structures or things that live in the standard libraries, you have to import the definitions from the standard library.
+
+After writing an import like this for a Lean module, the definitions from the module become available in the current Lean source file.
 
 ```lean
 import Std.Data.HashSet
 ```
 
-## Mathlib
+You might need to restart the language server of Lean in your editor after changing an import. Shortcut: `Ctrl + Shift + X`.
 
-Add dependency on Mathlib:<https://github.com/leanprover-community/mathlib4/wiki/Using-mathlib4-as-a-dependency>
+You can find the **import paths** by look at the relative path of the module to the `src/std` directory in the [Lean 4 repository](https://github.com/leanprover/lean4/tree/master/src/Std).
+
+_**Important**: References to the definitions in the standard library in the reference manual point to namespaces (not import paths for modules)._
+
+For example `HashSet` is defined in the namespace `Std.HashSet`, according to the reference manual, but you have to import it like `import Std.Data.HashSet`.
+
+### Building the project
+
+Successfull compilation of a project in Lean shows that all logical and mathematical statements expressed the type system are valid. To compile (build) the project, run:
+
+```bash
+lake build
+```
+
+## Mathematics
+
+### Mathlib
+
+A large superset of the standard library. It contains a lot of mathematical definitions and theorems, which are useful for writing mathematical proofs in Lean.
+
+### Mathlib installation
+
+To install Mathlib as a dependency to your current project, follow [Mathlib installation instructions](https://github.com/leanprover-community/mathlib4/wiki/Using-mathlib4-as-a-dependency).
 
 Inside the `lakefile.toml` file, add the following lines:
 
@@ -28,18 +72,29 @@ scope = "leanprover-community"
 lake update mathlib
 ```
 
-Search for the theorems that you need in Mathlib:
+### Searching mathematical facts
 
-- Loogle, Moogle
-- Directly in the browser: <https://leanprover-community.github.io/mathlib4_docs/Mathlib.html>
+The collection of definitions and theorems in Mathlib is large, so it is useful to have some tools to search for definitions and theorems:
 
-Import
+- Interactive [HTML pages](https://leanprover-community.github.io/mathlib4_docs/Mathlib.html) with cross-references and syntax highlighting.
+
+- Definition search engines provided by language server (and hosted online):
+  - Based on using meta-variables and the type system: [Loogle](https://loogle.lean-lang.org/).
+  - Search using natural language: [Moogle](https://moogle.ai/).
+
+Example of an import for a mathematical definition from Mathlib:
 
 ```lean
 import Mathlib.Algebra.MonoidAlgebra.Basic
 #check AddMonoidAlgebra.lift_def
 ```
 
-## Changing imports
+### Mathematical learning resources
 
-It seems like you have to rerun "restart file" when you change the imports in the file. Shortcut: `Ctrl + Shift + X`.
+Good resources to learn how to do (tranditional) formal mathematics in Lean:
+
+- [Mathematics in Lean](https://leanprover-community.github.io/mathematics_in_lean)
+
+## Puzzles
+
+Located inside the [`RiddleProofs`](./RiddleProofs) directory.
--- RiddleProofs/JealousMath.lean
+++ RiddleProofs/JealousMath.lean
@@ -3,37 +3,44 @@ import Init.WF
 /-!
 # River crossing puzzle:
 
-Three jealous mathematicians (A, B, C) and their three secret notebooks (a, b, c) must cross a river.
-The boat carries at most two items (people or notebooks).
-No mathematician may be left alone with another’s notebook unless the owner is present.
+Three jealous mathematicians and their three secret notebooks must cross a river.
+
+- The boat carries at most two items (people or notebooks).
+- No mathematician may be left alone with another’s notebook unless the owner is present.
 
 Can they all get across safely?
 -/
 
+def num_mathematicians: Nat := 3
 
 inductive RiverBank
-| left | right
-  deriving DecidableEq, Repr, Inhabited
+| left
+| right
+deriving DecidableEq, Repr, Inhabited
+
 
 open RiverBank
 
 structure State where
-  boat : RiverBank  -- Boat position
-  mathematicians : Vector RiverBank 3
-  notebooks : Vector  RiverBank 3
+  boat : RiverBank
+  mathematicians : Vector RiverBank num_mathematicians
+  notebooks : Vector  RiverBank num_mathematicians
   deriving Repr, DecidableEq
 
 -- Safety condition: no notebook is ever with another mathematician unless its owner is present
 -- (i.e., notebook i cannot be with mathematician j unless mathematician i is also present, for i ≠ j)
 def no_notebook_left_behind (s : State) : Prop :=
-  ∀ i j : Fin 3, (i ≠ j) →
+  ∀ i j : Fin num_mathematicians, (i ≠ j) →
     let Ni := s.notebooks[i]
     let Mj := s.mathematicians[j]
     let Mi := s.mathematicians[i]
     Ni = Mj → Mi = Mj
 
--- Whether a property is decidable depends on how it is defined. If it is built from decidable operations and quantifies only over finite/enumerable types, Lean can often infer it (with infer_instance), but it cannot always do so automatically without being told to try.
+/-
+## Decidability
 
+Whether a property is decidable depends on how it is defined. If it is built from decidable operations and quantifies only over finite/enumerable types, Lean can often infer it (with infer_instance), but it cannot always do so automatically without being told to try.
+ -/
 instance : DecidablePred no_notebook_left_behind := by
   unfold no_notebook_left_behind
   infer_instance
@@ -55,7 +62,7 @@ theorem final_safe: no_notebook_left_behind (final_state):= by
   rw [final_state]
   simp
 
--- There exists a sequence of states from initial to goal (trivial version)
+-- If we drop the constraint temporarily that all transfers have to happen step-wise and just look at the initial and final state, we should be able to prove that the two-state path is safe.
 theorem impossible_path_safe :
   ∃ (path : List State),
     path.head? = some  initial_state ∧
@@ -63,72 +70,117 @@ theorem impossible_path_safe :
     True :=
   ⟨[initial_state, final_state], rfl, rfl, trivial⟩
 
+
+
+
+
+section Helpers
+
+/-
+## Helpers / abstractions
+
+Let's model the way items or people travel across the river.
+ -/
+
+def move_mathematician (n : Fin num_mathematicians) (b : RiverBank) (s : State) : State :=
+{ s with mathematicians := s.mathematicians.set n b }
+
+def move_notebook (n : Fin num_mathematicians) (b : RiverBank) (s : State) : State :=
+{ s with notebooks := s.notebooks.set n b }
+
+def move_boat (b : RiverBank) (s : State) : State :=
+{ s with boat := b }
+
+
+-- To be able to use numerals like 4 for creating terms of type `Fin 4`, we have to implement a procedure to automatically determine `4 < num_mathematicians`. In this case it is just `decide`.
+instance : OfNat (Fin num_mathematicians) n where
+  ofNat := ⟨n % num_mathematicians, Nat.mod_lt n (by decide)⟩
+
+
+end Helpers
+
+
 section Solution
 
- def move_mathematician (n : Fin 3) (b : RiverBank) (s : State) : State :=
-  { s with mathematicians := s.mathematicians.set n b }
+def n_transfers : Nat := 11
 
- def move_notebook (n : Fin 3) (b : RiverBank) (s : State) : State :=
-  { s with notebooks := s.notebooks.set n b }
+/-
+## Obtaining solution
 
- def move_boat (b : RiverBank) (s : State) : State :=
-  { s with boat := b }
+In each step we will transfer a mathematician or a notebook across the river.
 
--- A solution as a vector of crossing functions (State → State)
-def intermediate_steps : Vector (State → State) 11 :=
+This problem is similar to the "jealous husbands" riddle. The solution is identical.
+See [Wikipedia](https://en.wikipedia.org/wiki/Missionaries_and_cannibals_problem).
+ -/
+def transfers : Vector (State → State) n_states :=
   Vector.ofFn (fun
-    | 0 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
-    | 1 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
-    | 2 => move_boat right ∘ move_notebook 1 right ∘ move_mathematician 1 right
-    | 3 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
-    | 4 => move_boat right ∘ move_notebook 2 right ∘ move_mathematician 2 right
-    | 5 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
-    | 6 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
-    | 7 => move_boat left ∘ move_notebook 1 left ∘ move_mathematician 1 left
-    | 8 => move_boat right ∘ move_notebook 1 right ∘ move_mathematician 1 right
-    | 9 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
-    | 10 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right)
+    | k => match k with
+      | _ => match k.val with
+        | 0 => move_boat right ∘ move_notebook 0  right ∘ move_mathematician 0 right
+        | 1 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
+        | 2 => move_boat right ∘ move_notebook 1 right ∘ move_mathematician 1 right
+        | 3 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
+        | 4 => move_boat right ∘ move_notebook 2 right ∘ move_mathematician 2 right
+        | 5 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
+        | 6 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
+        | 7 => move_boat left ∘ move_notebook 1 left ∘ move_mathematician 1 left
+        | 8 => move_boat right ∘ move_notebook 1 right ∘ move_mathematician 1 right
+        | 9 => move_boat left ∘ move_notebook 0 left ∘ move_mathematician 0 left
+        | 10 => move_boat right ∘ move_notebook 0 right ∘ move_mathematician 0 right
+        | _ => id)
 
 end Solution
 
+-- Number of states includes an extra final state.
+def n_states := n_transfers + 1
+
 namespace StructuralRecursion
 
-def intermediate_states_structural_rec : (n: Nat) -> n < 11 → State
+def intermediate_states_structural_rec : (n: Nat) -> n < n_states → State
   | 0, _ => initial_state
   | n+1, h =>
     let prev := intermediate_states_structural_rec n (Nat.lt_of_succ_lt h)
-    (intermediate_steps.get ⟨n, Nat.lt_of_succ_lt h⟩) prev
+    (transfers.get ⟨n, Nat.lt_of_succ_lt h⟩) prev
 
-def intermediate_states_structural : Fin 11 → State := (fun n =>  intermediate_states_structural_rec n.val n.isLt)
+def intermediate_states_structural : Fin n_states → State := (fun n =>  intermediate_states_structural_rec n.val n.isLt)
 
 end StructuralRecursion
 
 
 /-
-# Well-founded recusion
+## Well-founded recusion
 
 Some types of recursion may lead to unterminated recursion. Well-founded recursion is a recursion closure together with a proof that a certain recursive definition terminates.
 
 The result of `measure` allows argument j := <n+1,_> to fix to be compared against the recursive argument i:= <n,_> by mapping these arguments to the natural numbers.
 -/
 
-noncomputable def intermediate_states : Fin 11 → State :=
-  WellFounded.fix (measure (fun (i : Fin 11) => i.val)).wf
+noncomputable def intermediate_states : Fin n_states → State :=
+  WellFounded.fix (measure (fun (i : Fin n_states) => i.val)).wf
     (fun i rec =>
       match i with
       | ⟨0, _⟩ => initial_state
       | ⟨n+1, h⟩ =>
         let prev := rec ⟨n, Nat.lt_of_succ_lt h⟩ (Nat.lt_succ_self n)
-        (intermediate_steps.get ⟨n, Nat.lt_of_succ_lt h⟩) prev
+        (transfers.get ⟨n, Nat.lt_of_succ_lt h⟩) prev
     )
-
 -- `noncomputable` means not executable code is compiled
 
-example : intermediate_states 0 = initial_state := by
+
+-- Verify the start and end point are as expected.
+example : intermediate_states ⟨0, by decide⟩ = initial_state := by
   decide
 
-example : (intermediate_states  9) = final_state := by
+example : intermediate_states ⟨n_states - 1, by decide⟩ = final_state := by
   decide
 
-theorem all_states_safe : ∀ i : Fin 11, no_notebook_left_behind (intermediate_states  i) := by
+/-
+# Correctness of solution
+
+The main correctness theorem can be verified with `decide` because all involved functions are decidable.
+
+If you prefer a manual approach, you can use a more verbose forward or backward proof.
+ -/
+
+theorem all_states_safe : ∀ i : Fin n_states, no_notebook_left_behind (intermediate_states  i) := by
   decide
