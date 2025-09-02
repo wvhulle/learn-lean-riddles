e47e36573488ed61657e96cc21d08b6debd5cb60
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-09-02 17:09:29 +0200

    Simplify boards examples solvability

--- RiddleProofs/LightsOut/BoardExamples.lean
+++ RiddleProofs/LightsOut/BoardExamples.lean
@@ -1,5 +1,8 @@
 import RiddleProofs.LightsOut.Statement
 
+def buttonSet {m n : ℕ} (s : Finset (Button m n)) : ButtonSelection m n :=
+  fun btn => if btn ∈ s then 1 else 0
+
 section TwoByTwo
 
 /-- A simple 2×2 puzzle: single light at top-left corner (0,0)
@@ -32,21 +35,12 @@ theorem example2x2_press_00 :
     decide }
 
 theorem example2x2_solution :
-  applyButtons example2x2 ({(0, 0), (0, 1), (1, 0)} : Finset (Button 2 2)) = allOff := by
-  funext i j
-  fin_cases i <;> fin_cases j <;>
-  { simp only [applyButtons, effect, isAffected,
-    areAdjacent, example2x2, allOff]
-    simp
-    decide }
+  applyButtons example2x2 {(0, 0), (0, 1), (1, 0)} = allOff := by
+  decide
 
 theorem example2x2_solvable : isSolvable example2x2 := by
-  use fun btn => if btn ∈ ({(0, 0), (0, 1), (1, 0)} : Finset (Button 2 2)) then 1 else 0
-  simp [applySelection, fromVector, buttonMatrix, toVector]
-  funext i j
-  fin_cases i <;> fin_cases j <;>
-  { simp only [example2x2, allOff, effect, isAffected, areAdjacent]
-    decide }
+  use buttonSet {(0, 0), (0, 1), (1, 0)}
+  decide
 
 end TwoByTwo
 
@@ -67,12 +61,8 @@ def cross3x3 : LightState 3 3 :=
   Matrix.of fun i j => if i = 1 ∨ j = 1 then 1 else 0
 
 theorem cross3x3_solvable : isSolvable cross3x3 := by
-  use fun btn => if btn = (1, 1) then 1 else 0
-  simp [applySelection, fromVector, buttonMatrix, toVector]
-  funext i j
-  fin_cases i <;> fin_cases j <;>
-  { simp only [cross3x3, allOff, effect, isAffected, areAdjacent]
-    decide }
+  use buttonSet {(1, 1)}
+  decide
 
 
 end ThreeByThree
